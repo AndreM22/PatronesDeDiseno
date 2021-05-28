ce6e045205c9ae2bcd2cc306794ce98c70b7b55a
element222 <fabian.machicao@gmail.com>
2021-05-27 22:30:09 -0400

    Ejercicio Iterator Empresa Software utilizando HashMap

--- src/main/java/EjercicioIteratorEmpresaSoftware/Cliente.java
+++ src/main/java/EjercicioIteratorEmpresaSoftware/Cliente.java
@@ -1,14 +1,17 @@
 package EjercicioIteratorEmpresaSoftware;
 
+import java.util.HashMap;
 
 public class Cliente {
 
 	public static void main(String[] args) {
+		HashMap<Integer, String> empleados = new HashMap<Integer, String>();
+		int numeroDeEmpleado = 1;
 		ListaList lista = new ListaList();
 		ListaArray array = new ListaArray();
-		ListaVector vector= new ListaVector();
+		ListaVector vector = new ListaVector();
 		ListStack stack = new ListStack();
-		
+
 		lista.add("Antony");
 		lista.add("Marcelo");
 		lista.add("Pedro");
@@ -33,22 +36,38 @@ public class Cliente {
 		stack.add("Carol");
 		stack.add("Esther");
 
-        Iterator iterator = lista.iterator();
-        while (iterator.hasNext()){
-            System.out.println("Nombre empresa 1: "+iterator.next());
-        } 
-        iterator = array.iterator();
-        while (iterator.hasNext()){
-            System.out.println("Nombre empresa 2: "+iterator.next());
-        } 
-        iterator = vector.iterator();
-        while (iterator.hasNext()){
-            System.out.println("Nombre empresa 3: "+iterator.next());
-        } 
-        iterator = stack.iterator();
-        while (iterator.hasNext()){
-            System.out.println("Nombre empresa 4: "+iterator.next());
-        } 
+		Iterator iterator = lista.iterator();
+		String empleadoActual;
+		while (iterator.hasNext()) {
+			empleadoActual = (String) iterator.next();
+			System.out.println("Nombre empresa 1: " + empleadoActual);
+			empleados.put(numeroDeEmpleado, empleadoActual);
+			numeroDeEmpleado++;
+		}
+		iterator = array.iterator();
+		while (iterator.hasNext()) {
+			empleadoActual = (String) iterator.next();
+			System.out.println("Nombre empresa 2: " + empleadoActual);
+			empleados.put(numeroDeEmpleado, empleadoActual);
+			numeroDeEmpleado++;
+		}
+		iterator = vector.iterator();
+		while (iterator.hasNext()) {
+			empleadoActual = (String) iterator.next();
+			System.out.println("Nombre empresa 3: " + empleadoActual);
+			empleados.put(numeroDeEmpleado, empleadoActual);
+			numeroDeEmpleado++;
+		}
+		iterator = stack.iterator();
+		while (iterator.hasNext()) {
+			empleadoActual = (String) iterator.next();
+			System.out.println("Nombre empresa 4: " + empleadoActual);
+			empleados.put(numeroDeEmpleado, empleadoActual);
+			numeroDeEmpleado++;
+
+		}
+		
+		System.out.println(empleados.size()+" es el número de empleados");
 	}
 
 }
